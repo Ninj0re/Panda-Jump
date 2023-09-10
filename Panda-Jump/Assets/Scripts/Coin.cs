c857bacbe226f9478c40231543e0ab24355d4487
Cinar Okutan <110976036+Ninj0re@users.noreply.github.com>
2023-09-10 18:23:45 +0300

    Coin Spawner final

--- Panda-Jump/Assets/Scripts/Coin.cs
+++ Panda-Jump/Assets/Scripts/Coin.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     [SerializeField] private int coin;
+    [SerializeField] private CoinSpawner coinSpawner;
+    public static float spawnHeight;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerMovement pm = collision.gameObject.GetComponent<PlayerMovement>();
         if (pm != null)
         {
             pm.AddCoin(coin);
+            coinSpawner.Spawn(spawnHeight);
             Destroy(gameObject);
         }
+    }
+
+    public void AsignComponents(CoinSpawner cn)
+    {
+        coinSpawner = cn;
     }
 }
